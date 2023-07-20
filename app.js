2ce2526baff90edd2a060e7461fb6f79b883ee18
karl <oualidbahloul72@gmail.com>
2023-07-20 12:56:07 +0100

    this will be make changes on html file and css file

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-confirm("you want to see this page content ?");
+var k = confirm("you want to see this page content ?");
+if(k == true){
+    alert("you are in !")
+}
+if(k == false){
+    alert("you dont have a permition ! ")
+    h.style.display="none";
+    alert("you dont have any permiont for see the content of this page ! ")
+}
+function karl(){
+    
+}
